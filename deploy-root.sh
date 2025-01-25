7ff7ba2d51382050178013c755f0f8a9c5025679
jett09 <token_explorer@outlook.com>
2025-01-25 13:10:13 +0800

    update readme

--- deploy-root.sh
+++ deploy-root.sh
@@ -3,7 +3,3 @@
 dfx identity use ai-data
 
 dfx deploy --network=ic root --subnet=ejbmu-grnam-gk6ol-6irwa-htwoj-7ihfl-goimw-hlnvh-abms4-47v2e-zqe
-
-
-# dfx canister --network=ic call root set_governance_canister '(principal "xzhe2-gqaaa-aaaae-qajqa-cai")'
-# dfx canister --network=ic call root set_index_canister '(principal "x6gco-liaaa-aaaae-qajqq-cai")'
---
+++ update-root.sh
@@ -0,0 +1,6 @@
+
+
+dfx identity use ai-data
+
+dfx canister --network=ic call root set_governance_canister '(principal "xzhe2-gqaaa-aaaae-qajqa-cai")'
+dfx canister --network=ic call root set_index_canister '(principal "x6gco-liaaa-aaaae-qajqq-cai")'
